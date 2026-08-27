ffd4bc6947f1bbe5641c4dec3526bbf374830019
seulgi <piao516@naver.com>
2026-08-27 15:30:26 +0900

    fix: user.ts 중복선언 수정

--- src/types/user.ts
+++ src/types/user.ts
@@ -5,13 +5,9 @@ export interface UserMe {
   name: string;
 }
 
-export interface UserMe {
-  id: number;
-  email: string;
-  name: string;
-}
-
 export interface ChangePasswordRequest {
   currentPassword: string;
   newPassword: string;
 }
+
+
